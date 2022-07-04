8069525e691a32882ba53850c310a15fad64c068
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2022-07-04 14:54:56 +0300

    fix

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -2,6 +2,8 @@ const { User } = require("../../models/user");
 const bcrypt = require("bcryptjs");
 const { createError } = require("../../helpers");
 const gravatar = require("gravatar");
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = process.env;
 
 const signup = async (req, res) => {
   const { email, password } = req.body;
@@ -13,13 +15,18 @@ const signup = async (req, res) => {
   const salt = await bcrypt.genSalt(10);
   const hashPassword = await bcrypt.hash(password, salt);
   const avatarUrl = gravatar.url(email);
-
+  const payload = {
+    id: email,
+  };
+  const token = jwt.sign(payload, SECRET_KEY);
   const newUser = await User.create({
     ...req.body,
+    token,
     avatarUrl,
     password: hashPassword,
   });
   res.status(201).json({
+    token,
     user: {
       email: newUser.email,
       name: newUser.name,
