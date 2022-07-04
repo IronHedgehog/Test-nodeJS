0e48be07bf8e5d5b8d25f75949aa93d978973771
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2022-07-04 15:27:57 +0300

    fix

--- models/shop.js
+++ models/shop.js
@@ -4,9 +4,6 @@ const Joi = require("joi");
 const emailRegExp =
   /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,100})$/i;
 
-const phoneRegExp =
-  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
-
 const userNameRegExp = /^(?!.*\.\.)(?!\.)(?!.*\.$)(?!\d+$)[a-zA-Z0-9.]{2,12}$/;
 const orderSchema = Schema(
   {
@@ -14,15 +11,6 @@ const orderSchema = Schema(
       type: String,
       match: userNameRegExp,
       required: [true, "Set name for owner"],
-    },
-    phone: {
-      type: Number,
-      match: phoneRegExp,
-      required: [true, "Set phone for owner"],
-    },
-    address: {
-      type: String,
-      required: [true, "Set shop for product"],
     },
     email: {
       type: String,
@@ -43,8 +31,6 @@ const orderSchema = Schema(
 
 const addOrder = Joi.object({
   username: Joi.string().pattern(userNameRegExp).required(),
-  phone: Joi.number().required(),
-  address: Joi.string().required(),
   email: Joi.string().pattern(emailRegExp).required(),
   cart: Joi.array().items(Joi.object()),
 });
