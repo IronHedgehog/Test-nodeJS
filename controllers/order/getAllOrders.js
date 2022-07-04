91570d6d861925d23be1239e8559c610d31c4136
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2022-07-04 11:51:45 +0300

    fix

--- controllers/order/getAllOrders.js
+++ controllers/order/getAllOrders.js
@@ -4,7 +4,7 @@ const getAll = async (req, res) => {
   const { _id: owner } = req.user;
   // const { page = 1, limit = 1 } = req.query;
   // const skip = (page - 1) * limit;
-  const orders = await Order.find({}, "-createdAt -updatedAt", {
+  const orders = await Order.find({ owner }, "-createdAt -updatedAt", {
     // skip,
     // limit: Number(limit),
   });
