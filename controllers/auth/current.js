dd9b17eae7976691b931bd1465e0bb7a69955c11
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2022-07-06 12:32:39 +0300

    fix

--- controllers/auth/current.js
+++ controllers/auth/current.js
@@ -1,6 +1,6 @@
 const current = async (req, res) => {
-  const { email, name } = req.user;
-  res.status(200).json({ email, name });
+  const { email, name, address, phone } = req.user;
+  res.status(200).json({ email, name, address, phone });
 };
 
 module.exports = current;
